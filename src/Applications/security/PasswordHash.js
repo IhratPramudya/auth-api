eda7298479855a887904903315c0c12830b5bdcf
ihratpramudya <pramudyaihrat@gmail.com>
2023-05-10 19:56:57 +0700

    add Testing Domains Entities Infrastucture

---
+++ src/Applications/security/PasswordHash.js
@@ -0,0 +1,10 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable eol-last */
+class PasswordHash {
+  async hash(password) {
+    throw new Error('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+  }
+}
+ 
+module.exports = PasswordHash;
